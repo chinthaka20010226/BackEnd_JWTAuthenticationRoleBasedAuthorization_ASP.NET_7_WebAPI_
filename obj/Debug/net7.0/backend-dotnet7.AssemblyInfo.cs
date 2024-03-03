d9f50e512e60926b01bf2940cbca486ac2c621ac
chinthaka20010226 <prabathchinthaka361@gmail.com>
2024-03-03 14:44:32 +0530

    Constant class

--- obj/Debug/net7.0/backend-dotnet7.AssemblyInfo.cs
+++ obj/Debug/net7.0/backend-dotnet7.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend-dotnet7")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+485b0c49e2b14ce9bb56312f6ab7e80f78e6f8f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5371617fdaf3b58cb036a88c0729aefe96a4356")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend-dotnet7")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend-dotnet7")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
