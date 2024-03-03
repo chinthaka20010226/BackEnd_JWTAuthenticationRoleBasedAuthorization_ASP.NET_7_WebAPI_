497410bcf1837fccee0b3c31ff3ef07b4e909003
chinthaka20010226 <prabathchinthaka361@gmail.com>
2024-03-03 12:20:29 +0530

    Created ApplicationDbContext & Setup the program.cs, appsettings.json file

--- Program.cs
+++ Program.cs
@@ -1,8 +1,24 @@
+using backend_dotnet7.Core.DbContext;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+
+
+//DB
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("default");
+    options.UseSqlServer(connectionString);
+});
+
+
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
--- obj/Debug/net7.0/backend-dotnet7.AssemblyInfo.cs
+++ obj/Debug/net7.0/backend-dotnet7.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend-dotnet7")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f1c2323384f0a86fd52ff5212d4744f1096a89b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13c641f182680b023207a4a10c6f8ad423932fa6")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend-dotnet7")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend-dotnet7")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
