452247cda8dfd0425504f74b0b866cf2ed69b2a1
chinthaka20010226 <prabathchinthaka361@gmail.com>
2024-03-05 23:41:46 +0530

    Done the SeedRoles(), Register(), Login() -> AuthController

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,12 +1,52 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using backend_dotnet7.Core.Dtos.Auth;
+using backend_dotnet7.Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace backend_dotnet7.Controllers
 {
     public class AuthController : Controller
     {
-        public IActionResult Index()
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
         {
-            return View();
+            _authService = authService;
         }
+
+        // Route -> Seed role to DB
+        [HttpPost]
+        [Route("seed-roles")]
+        public async Task<IActionResult> SeedRoles()
+        {
+            var seedResult = await _authService.SeedRolesAsync();
+            return StatusCode(seedResult.StatusCode, seedResult.Message);
+        }
+
+        // Route -> Register
+        [HttpPost]
+        [Route("Register")]
+        public async Task<IActionResult> Register([FromBody] RegisterDto registerDto)
+        {
+            var registerResult = await _authService.RegisterAsync(registerDto);
+            return StatusCode(registerResult.StatusCode, registerResult.Message);
+        }
+
+        // Route -> Login
+        [HttpPost]
+        [Route("Login")]
+        public async Task<ActionResult<LoginServiceResponseDto>> Login([FromBody] LoginDto loginDto)
+        {
+            var loginResult = await _authService.LoginAsync(loginDto);
+
+            if(loginResult is null)
+            {
+                return Unauthorized("Your credentials are invalid. Please contact to an Admin");
+            }
+
+            return Ok(loginResult);
+        }
+
+        
+
     }
 }
