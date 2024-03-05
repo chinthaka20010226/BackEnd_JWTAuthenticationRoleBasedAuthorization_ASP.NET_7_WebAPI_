09d19ac274109dadb77c4db2b30542e47bfb6b02
chinthaka20010226 <prabathchinthaka361@gmail.com>
2024-03-06 00:20:19 +0530

    Done the AuthController

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,6 +1,9 @@
-﻿using backend_dotnet7.Core.Dtos.Auth;
+﻿using backend_dotnet7.Core.Constants;
+using backend_dotnet7.Core.Dtos.Auth;
 using backend_dotnet7.Core.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections;
 
 namespace backend_dotnet7.Controllers
 {
@@ -38,7 +41,7 @@ namespace backend_dotnet7.Controllers
         {
             var loginResult = await _authService.LoginAsync(loginDto);
 
-            if(loginResult is null)
+            if (loginResult is null)
             {
                 return Unauthorized("Your credentials are invalid. Please contact to an Admin");
             }
@@ -46,7 +49,81 @@ namespace backend_dotnet7.Controllers
             return Ok(loginResult);
         }
 
-        
+        // Route -> Update User Role
+        // Admin can change User Role
+        [HttpPost]
+        [Route("update-role")]
+        [Authorize(Roles = StaticUserRoles.ADMIN)]
+        public async Task<IActionResult> UpdateRole([FromBody] UpdateRoleDto updateRoleDto)
+        {
+            var updateRoleResult = await _authService.UpdateRoleAsync(User, updateRoleDto);
+
+            if (updateRoleResult.IsSucceed)
+            {
+                return Ok(updateRoleResult.Message);
+            }
+            else
+            {
+                return StatusCode(updateRoleResult.StatusCode, updateRoleResult.Message);
+            }
+        }
+
+        // Route -> getting data of a user from it's JWT
+        [HttpPost]
+        [Route("me")]
+        public async Task<ActionResult<LoginServiceResponseDto>> Me([FromBody] MeDto token)
+        {
+            try
+            {
+                var me = await _authService.MeAsync(token);
+                if(me is not null)
+                {
+                    return Ok(me);
+                }
+                else
+                {
+                    return Unauthorized("Invalid Token");
+                }
+            }
+            catch(Exception ex)
+            {
+                return Unauthorized("Invalid Token");
+            }
+        }
+
+        // Route -> List of all Users with details
+        [HttpGet]
+        [Route("users")]
+        public async Task<ActionResult<IEnumerable<UserInfoResult>>> GetUsersList()
+        {
+            var usersList = await _authService.GetUsersListAsync();
+            return Ok(usersList);
+        }
+
+        // Route -> Get a User by UserName
+        [HttpGet]
+        [Route("users/{userName}")] //[Route("users/{parameter}")]
+        public async Task<ActionResult<UserInfoResult>> GetUserDetailsByUserName([FromRoute] string userName)
+        {
+            var user = await _authService.GetUserDetailsByUserNameAsync(userName);
+            if (user is not null)
+            {
+                return Ok(user);
+            }
+            else
+            {
+                return NotFound("UserName not found");
+            }
+        }
+
+        // Route -> Get List of all usernames for send message
+        [HttpGet]
+        [Route("usernames")]
+        public async Task<ActionResult<IEnumerable<string>>> GetUserNamesList()
+        {
+            var userNames = await _authService.GetUsernameListAsync();
+            return Ok(userNames);
+        }
 
     }
 }
--- obj/Debug/net7.0/backend-dotnet7.AssemblyInfo.cs
+++ obj/Debug/net7.0/backend-dotnet7.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend-dotnet7")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ab5cbdd88bd3fee5275ba21828e5747713a8772")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+452247cda8dfd0425504f74b0b866cf2ed69b2a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend-dotnet7")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend-dotnet7")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
