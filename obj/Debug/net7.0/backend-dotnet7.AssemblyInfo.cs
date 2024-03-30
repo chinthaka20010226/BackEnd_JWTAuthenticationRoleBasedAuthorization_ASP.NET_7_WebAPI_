eb28af852e922696951d5a992ee2982ddd7b26e3
chinthaka20010226 <prabathchinthaka361@gmail.com>
2024-03-30 23:14:41 +0530

    Now, Project has big problem -> have to remove UserRoles Table -> User has not Several Roles

--- obj/Debug/net7.0/backend-dotnet7.AssemblyInfo.cs
+++ obj/Debug/net7.0/backend-dotnet7.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend-dotnet7")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+158acc485f0bf885e048951f64da27678591cf87")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+babf5fcc6bcae51a50f6d5e37500d9c83e7d4211")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend-dotnet7")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend-dotnet7")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
